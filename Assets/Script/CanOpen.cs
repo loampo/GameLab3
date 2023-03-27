ba067b7c1dafaba6e675c8196643e7a21ef29e75
loampo <loampo1@gmail.com>
2023-03-27 15:01:38 +0200

    Door open and collider fix
    Door open and collider fix

--- Assets/Script/CanOpen.cs
+++ Assets/Script/CanOpen.cs
@@ -4,21 +4,14 @@ using UnityEngine;
 
 public class CanOpen : MonoBehaviour
 {
-
-    public Animator m_animator;
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+    
+    private void OnTriggerEnter(Collider other)
     {
-        m_animator = GetComponent<Animator>();
-        m_animator.enabled = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (PlayerInformation.m_instance.m_RedKey == true)
+        if (other.gameObject.CompareTag("Player") && PlayerInformation.m_instance.m_RedKey == true)
         {
-            m_animator.enabled = true;
+            Debug.Log("open");
+            animator.SetBool("Open",true);            
         }
     }
 }
