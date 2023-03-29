469db2ad2092b5998a7d9eabc896522550f484e7
Lollo48 <loory4895@gmail.com>
2023-03-29 23:45:34 +0200

    add models
    add models

--- Assets/Script/Doors/ShootableBox.cs
+++ Assets/Script/Doors/ShootableBox.cs
@@ -7,22 +7,19 @@ public class ShootableBox : MonoBehaviour, IDamageable, IShootableBoox
 {
     public int m_currentHealth;
 
+    public Animator animator;
 
     public void DamageBulletDoor(int damageAmount)
     {
         //subtract damage amount when Damage function is called
         m_currentHealth -= damageAmount;
-
+        if (m_currentHealth <= 0) animator.SetBool("isBroken", true);
         //Check if health has fallen below zero
-        if (m_currentHealth <= 0)
-        {
 
-            Destroy(gameObject);
-
-        }
     }
 
-   
+  
+
 }
 interface IShootableBoox
 {
