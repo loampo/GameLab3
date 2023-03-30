5f617337d0df91a9c821a0b3007072820f40b25f
loampo <loampo1@gmail.com>
2023-03-31 01:04:24 +0200

    door explosion and new lightmap

--- Assets/Script/Doors/ShootableBox.cs
+++ Assets/Script/Doors/ShootableBox.cs
@@ -6,18 +6,26 @@ using UnityEngine;
 public class ShootableBox : MonoBehaviour, IDamageable, IShootableBoox
 {
     public int m_currentHealth;
-
+    public GameObject m_effect; //animation
     public Animator animator;
+    public Transform m_doorLocation;
+    public bool m_needEffect;
 
     public void DamageBulletDoor(int damageAmount)
     {
         //subtract damage amount when Damage function is called
         m_currentHealth -= damageAmount;
-        if (m_currentHealth <= 0) animator.SetBool("isBroken", true);
         //Check if health has fallen below zero
-
+        if (m_currentHealth <= 0 && m_needEffect)
+        {
+            animator.SetBool("isBroken", true);
+            GameObject a = Instantiate(m_effect, m_doorLocation.position, Quaternion.identity); //Instantiate the animation 
+            Destroy(a, 2f); //destroy the animation after 2 seconds
+        }else if (m_currentHealth <= 0 && !m_needEffect)
+        {
+            animator.SetBool("isBroken", true);
+        }
     }
-
   
 
 }
